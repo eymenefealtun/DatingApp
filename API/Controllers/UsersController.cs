70dda9a7c0dd477a225d83611888aa0658a2e47d
eymenefealtun <eymenefealtun@hotmail.com>
2023-09-08 13:56:20 +0300

    feat: end of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //https://localhost:5001/api/users
+public class UsersController : Controller
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet] // Http request
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() // Task represents an asynchronous operation that can retun a value.
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")] // endpoit     //https://localhost:5001/api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+}
