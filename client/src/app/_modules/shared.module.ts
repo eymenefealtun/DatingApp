838bbdeef5cf916e8727bbb259a072d5b223f73b
eymenefealtun <eymenefealtun@hotmail.com>
2023-09-17 09:59:27 +0300

    End of section 6

---
+++ client/src/app/_modules/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
+@NgModule({ // we created this with 'ng g module _nodules/shared --flat' command and using this for importing only 3rd party libraries in a one folder which is going to prevent app.module.ts to become more complex
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
